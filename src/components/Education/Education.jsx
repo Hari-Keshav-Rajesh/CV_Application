bcd309bdc07ec335f73a54f940c23cc9eef48ca9
Hari-Keshav-Rajesh <harikeshav.rajesh@gmail.com>
2023-09-01 16:11:32 +0530

    Education form completed

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -4,29 +4,43 @@ import "./Education.css"
 
 const Education = (props) => {
 
-    const [degree,setDegree] = useState("")
-    const [university,setUniversity] = useState("")
-    const [start,setStart] = useState("")
-    const [end,setEnd] =  useState("")
+    const [degree,setDegree] = useState([])
+    const [university,setUniversity] = useState([])
+    const [start,setStart] = useState([])
+    const [end,setEnd] =  useState([])
 
-    const formSubmit = (event) => {
-        event.preventDefault();
-        
-        if (degree && university && start && end) {
-            props.setEducationList([
-                ...props.educationList,
-                { degree: degree, university: university, start: start, end: end }
-            ]);
-        } else {
-            alert("Please fill Education details");
+
+    const formDelete = (event,id,index) => {
+
+        event.preventDefault()
+
+        if(props.education.length===1){
+            alert("Atleast one element mandatory")
         }
-    }
-
-    const formDelete = (id) => {
-        const updatedList = props.education.filter(
-          (education) => education.id !== id
-        );
-        props.setEducation(updatedList);
+        else{
+            const updatedList = props.education.filter(
+                (education) => education.id !== id
+              );
+              props.setEducation(updatedList);
+      
+              const deleteIndex = index
+              
+              const List = props.educationList.filter((_, index) => index !== deleteIndex);
+              props.setEducationList(List)
+      
+              const newDegree = degree.filter((_, index) => index !== deleteIndex);
+              setDegree(newDegree)
+      
+              const newUni = university.filter((_, index) => index !== deleteIndex);
+              setUniversity(newUni)
+      
+              const newStart = start.filter((_, index) => index !== deleteIndex);
+              setStart(newStart)
+      
+              const newEnd = end.filter((_, index) => index !== deleteIndex);
+              setEnd(newEnd)
+      
+        }
       };
 
     
@@ -47,44 +61,67 @@ const Education = (props) => {
                         <input className="degree"
                         type="text"
                         placeholder="Degree"
-                        value = {degree}
-                        onChange={(event) => setDegree(event.target.value)}>
+                        value = {degree[index]}
+                        onChange={(event) => {
+                            let deg = degree
+                            deg[index] = event.target.value
+                            setDegree(deg)
+                            let list = [...props.educationList]
+                            list[index] = {degree:degree[index],university:university[index],start:start[index],end:end[index]}
+                            props.setEducationList(list)
+                        }}>
                         </input>
 
                         <input className="university"
                         type="text"
                         placeholder="University"
-                        value = {university}
-                        onChange={(event) => setUniversity(event.target.value)}>
+                        value = {university[index]}
+                        onChange={(event) => {
+                            let uni = university
+                            uni[index] = event.target.value
+                            setUniversity(uni)
+                            let list = [...props.educationList]
+                            list[index] = {degree:degree[index],university:university[index],start:start[index],end:end[index]}
+                            props.setEducationList(list)
+                        }}>
                         </input>
 
                         <input className="start"
                         type="text"
                         placeholder="Start Year"
-                        value = {start}
-                        onChange={(event) => setStart(event.target.value)}>
+                        value = {start[index]}
+                        onChange={(event) => {
+                            let st = start
+                            st[index] = event.target.value
+                            setStart(st)
+                            let list = [...props.educationList]
+                            list[index] = {degree:degree[index],university:university[index],start:start[index],end:end[index]}
+                            props.setEducationList(list)
+                        }}>
                         </input>
 
                         <input className="end"
                         type="text"
                         placeholder="End Year"
-                        value = {end}
-                        onChange={(event) => setEnd(event.target.value)}>
+                        value = {end[index]}
+                        onChange={(event) => {
+                            let eend = end
+                            eend[index] = event.target.value
+                            setEnd(eend)
+                            let list = [...props.educationList]
+                            list[index] = {degree:degree[index],university:university[index],start:start[index],end:end[index]}
+                            props.setEducationList(list)
+                        }}>
                         </input>
 
                         <button className="educationDelete"
-                        onClick={() => formDelete(education.id,index)}>
+                        onClick={(event) => formDelete(event,education.id,index)}>
                         Delete
                         </button>
 
-                        <button className="educationSubmit"
-                        onClick={formSubmit}
-                        type="submit">
-                        Save
-                        </button>
 
                         <button className="educationAdd"
-                        onClick={formAdd}>
+                        onClick={(event) => formAdd(event)}>
                         Add
                         </button>
 
