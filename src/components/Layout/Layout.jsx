9cfcda955627e5aa807348ed94784c7ad2148b10
Hari-Keshav-Rajesh <harikeshav.rajesh@gmail.com>
2023-09-06 23:02:32 +0530

    Fixed button sizes

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,49 +1,54 @@
-import { useState } from 'react'
-import './Layout.css'
-import topLayoutIcon from '../../assets/Layout Icons/topLayoutIcon.png'
-import leftLayoutIcon from '../../assets/Layout Icons/leftLayoutIcon.png'
+import { useState } from "react"
+import "./Layout.css"
+import topLayoutIcon from "../../assets/Layout Icons/topLayoutIcon.png"
+import leftLayoutIcon from "../../assets/Layout Icons/leftLayoutIcon.png"
 
 const Layout = (props) => {
-    const [topClick, setTopClick] = useState(true)
-    const [leftClick, setLeftClick] = useState(false)
+
+    const [topClick,setTopClick] = useState(true)
+    const [leftClick,setLeftClick] = useState(false)
 
     const setLayoutTop = () => {
-        props.setLayout('top')
+        props.setLayout("top");
         setTopClick(true)
         setLeftClick(false)
-    }
-
-    const setLayoutLeft = () => {
-        props.setLayout('left')
+      };
+    
+      const setLayoutLeft = () => {
+        props.setLayout("left");
         setTopClick(false)
         setLeftClick(true)
-    }
-
-    const clickBorder = (click) => {
-        if (click === true) {
-            return {
-                borderWidth: '1vh',
+      };
+      
+      const clickBorder = (click) => {
+            if(click===true){
+                return{
+                    borderWidth: '1vh',
+                }
             }
-        }
-    }
+      }
 
-    return (
+    return(
         <div className="layoutBlock">
+
             <h1 className="layoutHeader">Layout Options</h1>
 
             <div className="layoutButtons">
+
                 <button onClick={setLayoutTop} style={clickBorder(topClick)}>
                     <div>
-                        <img src={topLayoutIcon} alt="Top" />
+                        <img src={topLayoutIcon} alt="Top"/>
                     </div>
                 </button>
 
                 <button onClick={setLayoutLeft} style={clickBorder(leftClick)}>
                     <div>
-                        <img src={leftLayoutIcon} alt="Left" />
+                        <img src={leftLayoutIcon} alt="Left"/>
                     </div>
                 </button>
+
             </div>
+
         </div>
     )
 }
